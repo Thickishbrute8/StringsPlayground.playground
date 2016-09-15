6e1cd0532ca95c68fb28fda290fbea8502b4d6a8
Britton Perry <bper5656@740s-pl-132.local>
2016-09-15 09:30:46 -0600

    First Playground

---
+++ Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let string1 = "Greetings "
+let string2 = "traveler"
+var welcome = string1 + string2
+print(welcome)
+
+let truu = "😂"
+print(truu)
+
+let 👋🏽 = "boi"
+print(👋🏽)
+
+let boi = "👋🏽"
+
+let lifeIsGood = false
+if lifeIsGood
+{
+    print("Life is worth living!")
+}
+else
+{
+    print("kms")
+}
+
+
+
+
+
